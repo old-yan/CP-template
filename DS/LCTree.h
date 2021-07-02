aee34628175c8cd2144a54d1455ec2525923f812
old-yan <oldyan_zh@163.com>
2021-07-02 09:17:22 +0800

    lctree update

--- DS/LCTree.h
+++ DS/LCTree.h
@@ -2,27 +2,25 @@
 #include <iostream>
 #include <memory.h>
 using namespace std;
-#define OLDYAN_DLTREE
+#define OLDYAN_LCTREE
 
 //李超线段树
 #define LCTREEDEPTH 17
 template<class T,class Pick,class PartPick>
 class LCTree{
-    T val[1<<LCTREEDEPTH],default_val;
+    T val[1<<(LCTREEDEPTH+1)],default_val;
     int X,Y;
     Pick pick;
     PartPick partpick;
     void push_down(int i,int l,int r,T _val){
-        int d=31-__builtin_clz(i),cl=(i&(1<<d)-1)<<(Y-d),cr=((i&(1<<d)-1)+1)<<(Y-d);
+        int d=31-__builtin_clz(i),cl=(i&(1<<d)-1)<<(Y-d),cr=(((i&(1<<d)-1)+1)<<(Y-d))-1;
         l=max(l,cl),r=min(r,cr);
-        if(r<cl||l>cr)return;
         if(l==cl&&r==cr){
             int res=partpick(_val,val[i],l,r);
-            if(res&1)val[i]=_val;
-            if(i<X&&res&2)push_down(i<<1,l,r,_val);
-            if(i<X&&res&4)push_down(i<<1^1,l,r,_val);
+            if(res&1)swap(val[i],_val);
+            if(i<X)push_down(i<<1^(res>>1&1),l,r,_val);
         }
-        else{
+        else if(l<=cr&&r>=cl){
             push_down(i<<1,l,r,_val);
             push_down(i<<1^1,l,r,_val);
         }
@@ -32,7 +30,7 @@ public:
         reset(n);
     }
     void reset(int n){
-        Y=(32--__builtin_clz(n-1)),X=1<<Y;
+        Y=(32-__builtin_clz(n-1)),X=1<<Y;
         fill(val,val+(X<<1),default_val);
     }
     void step(int l,int r,T _val){
@@ -40,7 +38,7 @@ public:
     }
     T operator[](int i){
         T res=default_val;
-        for(i+=X;i;i>>=1)if(pick(val[i],res,i))res=val[i];
+        for(int j=i+X;j;j>>=1)if(pick(val[j],res,i))res=val[j];
         return res;
     }
 };
