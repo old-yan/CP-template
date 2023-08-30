7b7edccdc666cfd29b9897682a01b9559312d9f7
old-yan <oldyan_zh@163.com>
2023-08-30 21:30:02 +0800

    indepedent BiTrie

--- DS/BiTrie.h
+++ DS/BiTrie.h
@@ -1,80 +1,141 @@
+/*
+最后修改:
+20230830
+测试环境:
+gcc11.2,c++11
+clang12.0,C++11
+msvc14.2,C++14
+*/
 #ifndef __OY_BITRIE__
 #define __OY_BITRIE__
 
 #include <algorithm>
-#include <tuple>
-#include "../STR/Trie.h"
+#include <cstdint>
+#include <functional>
+#include <numeric>
 
 namespace OY {
-    struct BiTrieDefaultInfo {};
-    template <typename _Tp, uint8_t _L>
-    struct BiTrieIteration {
-        _Tp m_number;
-        struct BiTrieIterator {
-            _Tp number;
-            uint32_t index;
-            BiTrieIterator(_Tp _number, uint32_t _index) : number(_number), index(_index) {}
-            BiTrieIterator &operator++() {
-                index--;
-                return *this;
-            }
-            BiTrieIterator operator+(uint32_t __i) const { return BiTrieIterator(number, index - __i); }
-            bool operator*() const { return number >> index & 1; }
-            bool operator==(const BiTrieIterator &_other) const { return !~index; }
-            bool operator!=(const BiTrieIterator &_other) const { return ~index; }
+    namespace BinaryTrie {
+        using size_type = uint32_t;
+        struct DefaultInfo {
+            bool m_isEnd;
         };
-        BiTrieIteration(_Tp __number) : m_number(__number) {}
-        BiTrieIterator begin() const { return BiTrieIterator(m_number, _L - 1); }
-        BiTrieIterator end() const { return BiTrieIterator(m_number, -1); }
-    };
-    template <typename _Tp = uint32_t, uint8_t _L = 30, typename _Info = BiTrieDefaultInfo, uint32_t _PoolSize = 2000000>
-    struct BiTrie {
-        struct BiTrieMapping {
-            static constexpr int range() { return 2; }
-            uint32_t operator()(bool __c) const { return __c; }
+        template <typename Tp, size_type L>
+        struct NumberInteration {
+            Tp m_number;
+            struct NumberInterator {
+                Tp m_number;
+                size_type m_index;
+                NumberInterator(Tp number, size_type index) : m_number(number), m_index(index) {}
+                NumberInterator &operator++() {
+                    m_index--;
+                    return *this;
+                }
+                size_type operator*() const { return m_number >> m_index & 1; }
+                bool operator==(const NumberInterator &rhs) const { return !~m_index; }
+                bool operator!=(const NumberInterator &rhs) const { return ~m_index; }
+            };
+            NumberInteration(Tp number) : m_number(number) {}
+            NumberInterator begin() const { return NumberInterator(m_number, L - 1); }
+            NumberInterator end() const { return NumberInterator(m_number, -1); }
         };
-        static inline Trie<BiTrieMapping, _Info, _PoolSize> s_trie;
-        using TrieNode = typename Trie<BiTrieMapping, _Info, _PoolSize>::TrieNode;
-        static void Init() { s_trie = Trie<BiTrieMapping, _Info, _PoolSize>(); }
-        static TrieNode Insert(const BiTrieIteration<_Tp, _L> &__numberIteration) { return s_trie.insert(__numberIteration); }
-        static std::pair<TrieNode, _Tp> QueryMaxSame(_Tp __number) {
-            TrieNode cur = s_trie.m_root;
-            _Tp res(0);
-            for (bool c : BiTrieIteration<_Tp, _L>(__number)) {
-                res *= 2;
-                if (TrieNode child = cur.child(c)) {
-                    cur = child;
-                    res++;
-                } else
-                    cur = cur.child(c ^ 1);
+        template <typename Tp = uint32_t, size_type L = 30, typename Info = DefaultInfo, BinaryTrie::size_type MAX_NODE = 1 << 20>
+        struct Tree {
+            struct iterator {
+                size_type m_index;
+                static iterator newNode() {
+                    iterator res;
+                    res.m_index = s_use_count++;
+                    return res;
+                }
+                static iterator newNode(iterator parent, size_type i) {
+                    s_parent[s_use_count].m_index = parent, s_child[parent.m_index][i].m_index = s_use_count;
+                    return newNode();
+                }
+                iterator &parent() { return s_parent[m_index]; }
+                iterator &child(size_type i) { return s_child[m_index][i]; }
+                iterator &child(size_type i) const { return s_child[m_index][i]; }
+                iterator &get_child(size_type i) {
+                    if (!s_child[m_index][i]) s_child[m_index][i] = newNode(*this, i);
+                    return s_child[m_index][i];
+                }
+                template <typename Iterator>
+                iterator insert(Iterator first, Iterator last) {
+                    if (first == last) return *this;
+                    iterator ch = get_child(*first);
+                    return ch.insert(++first, last);
+                }
+                template <typename Sequence>
+                iterator insert(const Sequence &sequence) { return insert(sequence.begin(), sequence.end()); }
+                template <typename Iterator>
+                iterator find(Iterator first, Iterator last) const {
+                    if (first == last) return *this;
+                    iterator ch = child(*first);
+                    if (ch.m_index) return ch.find(++first, last);
+                    return iterator{};
+                }
+                template <typename Sequence>
+                iterator find(const Sequence &sequence) const { return find(sequence.begin(), sequence.end()); }
+                Info *operator->() const { return s_info + m_index; }
+                Info &operator*() { return s_info[m_index]; }
+                explicit operator bool() const { return m_index; }
+                operator size_type() const { return m_index; }
+            };
+            static iterator s_parent[MAX_NODE], s_child[MAX_NODE][2];
+            static Info s_info[MAX_NODE];
+            static size_type s_use_count;
+            iterator m_root;
+            Tree() : m_root(iterator::newNode()) {}
+            iterator insert(Tp number) { return m_root.insert(NumberInteration<Tp, L>(number)); }
+            std::pair<iterator, Tp> query_max_same(Tp number) {
+                iterator cur = m_root;
+                Tp res{};
+                for (size_type c : NumberInteration<Tp, L>(number)) {
+                    res *= 2;
+                    if (iterator child = cur.child(c)) {
+                        cur = child;
+                        res++;
+                    } else
+                        cur = cur.child(c ^ 1);
+                }
+                return {cur, res};
             }
-            return {cur, res};
-        }
-        static std::pair<TrieNode, _Tp> QueryMaxBitxor(_Tp __number) {
-            __number ^= (_Tp(1) << _L) - 1;
-            TrieNode cur = s_trie.m_root;
-            _Tp res(0);
-            for (bool c : BiTrieIteration<_Tp, _L>(__number)) {
-                res *= 2;
-                if (TrieNode child = cur.child(c)) {
-                    cur = child;
-                    res++;
-                } else
-                    cur = cur.child(c ^ 1);
+            std::pair<iterator, Tp> query_max_bitxor(Tp number) {
+                number ^= L < sizeof(Tp) << 3 ? (Tp(1) << L) - 1 : Tp(-1);
+                iterator cur = m_root;
+                Tp res{};
+                for (size_type c : NumberInteration<Tp, L>(number)) {
+                    res *= 2;
+                    if (iterator child = cur.child(c)) {
+                        cur = child;
+                        res++;
+                    } else
+                        cur = cur.child(c ^ 1);
+                }
+                return {cur, res};
             }
-            return {cur, res};
-        }
-        static _Tp QueryLeafValue(TrieNode __leaf) {
-            TrieNode cur = __leaf;
-            _Tp res(0);
-            for (uint32_t i = 0; i < _L; i++) {
-                TrieNode parent = cur.parent();
-                if (cur == parent.child(1)) res |= _Tp(1) << i;
-                cur = parent;
+            Tp query_leaf_value(iterator leaf) {
+                iterator cur = leaf;
+                Tp res{};
+                for (size_type i = 0; i < L; i++) {
+                    iterator parent = cur.parent();
+                    if (cur == parent.child(1)) res |= Tp(1) << i;
+                    cur = parent;
+                }
+                return res;
             }
-            return res;
-        }
-    };
+        };
+        template <typename Tp, size_type L, typename Info, size_type MAX_NODE>
+        typename Tree<Tp, L, Info, MAX_NODE>::iterator Tree<Tp, L, Info, MAX_NODE>::s_parent[MAX_NODE];
+        template <typename Tp, size_type L, typename Info, size_type MAX_NODE>
+        typename Tree<Tp, L, Info, MAX_NODE>::iterator Tree<Tp, L, Info, MAX_NODE>::s_child[MAX_NODE][2];
+        template <typename Tp, size_type L, typename Info, size_type MAX_NODE>
+        Info Tree<Tp, L, Info, MAX_NODE>::s_info[MAX_NODE];
+        template <typename Tp, size_type L, typename Info, size_type MAX_NODE>
+        size_type Tree<Tp, L, Info, MAX_NODE>::s_use_count = 1;
+    }
+    template <typename Tp = uint32_t, BinaryTrie::size_type L = 30, typename Info = BinaryTrie::DefaultInfo, BinaryTrie::size_type MAX_NODE = 1 << 20>
+    using BiTrie = BinaryTrie::Tree<Tp, L, Info, MAX_NODE>;
 }
 
 #endif
--- DS/BiTrie.md
+++ DS/BiTrie.md
@@ -4,51 +4,49 @@
 
 ### 二、模板功能
 
-​	`BiTrie` 建立在 `Trie` 的基础上。 `BiTrie` 使用类内静态函数进行初始化、插入数字、查找树中最大异或点等操作。实际上，`BiTrie` 就是一颗动态开点线段树，只不过需要一些特定的接口。能用 `BiTrie` 解决的问题往往可以用动态开点线段树解决。
-
 
 #### 1.初始化
 
 1. 数据类型
 
-   模板参数 `typename _Tp` ，表示字典树中的数字类型，默认为 `uint32_t` 。
-
-   模板参数 `uint8_t _L` ，表示对每个数字的 `[0, _L)` 位，从高位到低位进行考虑。默认为 `30` 。
+   类型设定 `size_type = uint32_t` ，表示树中下标类型。
    
-   模板参数 `typename _Info` ，表示结点维护的信息的类。默认为 `BiTrieDefaultInfo` 。 
+   模板参数 `typename Tp` ，表示字典树中的数字类型，默认为 `uint32_t` 。
 
-   模板参数 `uint32_t _PoolSize` ，表示节点内存池大小。默认为 `2000000` 。 
+   模板参数 `size_type L` ，表示对每个数字的 `[0, L)` 位，从高位到低位进行考虑。默认为 `30` 。
+   
+   模板参数 `typename Info` ，表示结点维护的信息的类。默认为 `DefaultInfo` 。 
+
+   模板参数 `size_type MAX_NODE` ，表示节点内存池大小。默认为 `1<<20` 。 
 
 2. 时间复杂度
 
    $O(1)$ 。
+
+3. 备注
+
+    `01` 字典树本质和线段树一样，无非是在树中下降时，走左路还是走右路的选择比较自由。基于此，本数据结构中设计了迭代器 `iterator` 这一树中的类，通过迭代器可以较为方便地在树中上下移动、读写结点值、进行各种自由操作。
    
 
 #### 2.插入元素
 
 1. 数据类型
 
-   输入参数 `const BiTrieIteration<_Tp,_L> &__number` ，表示要插入的数字。
+   输入参数 `Tp number` ，表示要插入的数字。
 
-   返回类型 `TrieNode` ，表示插入元素后的结果点。
+   返回类型 `iterator` ，表示插入元素后的结果点。
 
 2. 时间复杂度
 
    $O(L)$ ，即关注的二进制长度。。
 
-3. 备注
-
-   传递的参数可以是 `_Tp` 类型。
-
-   插入顺序为，从前往后逐个插入字符。
-
 #### 3.查询最大相同值
 
 1. 数据类型
 
-   输入参数 `_Tp __number` ，表示要查询的数字是 `__number` 。
+   输入参数 `Tp number` ，表示要查询的数字。
 
-   返回类型 `std::pair<TrieNode, _Tp>` ，前者为适配的叶结点，后者为适配情况下的逐位判定相同与否的 `mask` 。
+   返回类型 `std::pair<iterator, Tp>` ，前者为适配的叶结点，后者为适配情况下的逐位判定相同与否的 `mask` 。
 
 2. 时间复杂度
 
@@ -62,9 +60,9 @@
 
 1. 数据类型
 
-   输入参数 `_Tp __number` ，表示要查询的数字是 `__number` 。
+   输入参数 `Tp number` ，表示要查询的数字。
 
-   返回类型 `std::pair<TrieNode, _Tp>` ，前者为适配的叶结点，后者为适配情况下的按位异或值。
+   返回类型 `std::pair<iterator, _Tp>` ，前者为适配的叶结点，后者为适配情况下的按位异或值。
 
 2. 时间复杂度
 
@@ -78,9 +76,9 @@
 
 1. 数据类型
 
-   输入类型 `TrieNode __leaf` ，表示要查询的叶结点。
+   输入类型 `iterator leaf` ，表示要查询的叶结点。
 
-   返回类型 `_Tp` ，表示该叶结点对应的数字。
+   返回类型 `Tp` ，表示该叶结点对应的数字。
 
 2. 时间复杂度
 
@@ -93,21 +91,27 @@
 #include "IO/FastIO.h"
 
 int main() {
-    //可以用静态方法初始化，而无需实例对象
-    OY::BiTrie<uint32_t, 5>::Init();
+    OY::BiTrie<uint32_t, 5> S;
 
-    //可以用静态方法插入数字
-    OY::BiTrie<uint32_t, 5>::Insert(15);
-    OY::BiTrie<uint32_t, 5>::Insert(1);
-    OY::BiTrie<uint32_t, 5>::Insert(9);
-    OY::BiTrie<uint32_t, 5>::Insert(14);
+    // 插入数字
+    S.insert(15);
+    S.insert(1);
+    S.insert(9);
 
-    //查询和 5 有最大相同值的数
-    auto [leaf, mask] = OY::BiTrie<uint32_t, 5>::QueryMaxSame(5);
-    cout << "5 and " << OY::BiTrie<uint32_t, 5>::QueryLeafValue(leaf) << " 's common bitmask :" << mask << endl;
-    //查询最大异或
-    auto [leaf2, mask2] = OY::BiTrie<uint32_t, 5>::QueryMaxBitxor(5);
-    cout << "5 and " << OY::BiTrie<uint32_t, 5>::QueryLeafValue(leaf2) << " 's bitxor result :" << mask2 << endl;
+    // 插入数字的返回值是一个迭代器，可以拿来做操作
+    auto insert_leaf = S.insert(14);
+    insert_leaf->m_isEnd = true;
+
+    // 查询和 5 有最大相同值的数
+    auto res1 = S.query_max_same(5);
+    auto leaf1 = res1.first;
+    auto mask1 = res1.second;
+    cout << "5 and " << S.query_leaf_value(leaf1) << " 's common bitmask :" << mask1 << endl;
+    // 查询最大异或
+    auto res2 = S.query_max_bitxor(5);
+    auto leaf2 = res2.first;
+    auto mask2 = res2.second;
+    cout << "5 and " << S.query_leaf_value(leaf2) << " 's bitxor result :" << mask2 << endl;
 }
 ```
 
